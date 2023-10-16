df047afc4d1c57170b24e3f3e0b1be49a03b1fc0
Devendra-Reddy-420 <136948121+Devendra-Reddy-420@users.noreply.github.com>
2023-10-16 22:45:12 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,8 @@
+def call(ip,name,warFile){
+  sshagent([name]) {
+                        // copy warfile onto tomcat
+                        sh "scp -o StrictHostKeyChecking=no target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
+                        sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
+                        sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
+  }
+}
